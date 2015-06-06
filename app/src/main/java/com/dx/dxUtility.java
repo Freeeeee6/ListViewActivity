ea74033c6740777c6bc7510ab24ee724ec69d193
Freeeeee6 <szdxSylar@gmail.com>
2015-06-06 15:54:11 +0800

    dxUtility增加LOG相关方法

--- app/src/main/java/com/dx/dxUtility.java
+++ app/src/main/java/com/dx/dxUtility.java
@@ -1,6 +1,7 @@
 package com.dx;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -19,6 +20,43 @@ public final class dxUtility {
 		// TODO Auto-generated constructor stub
 	}
 
+	// Log
+	public class LogUtil {
+		public static final int VERBOSE = 1;
+		public static final int DEBUG = 2;
+		public static final int INFO = 3;
+		public static final int WARN = 4;
+		public static final int ERROR = 5;
+		public static final int NOTHING = 6;
+		public static final int LEVEL = VERBOSE;
+		public void v(String tag, String msg) {
+			if (LEVEL <= VERBOSE) {
+				Log.v(tag, msg);
+			}
+		}
+		public void d(String tag, String msg) {
+			if (LEVEL <= DEBUG) {
+				Log.d(tag, msg);
+			}
+		}
+		public void i(String tag, String msg) {
+			if (LEVEL <= INFO) {
+				Log.i(tag, msg);
+			}
+		}
+		public void w(String tag, String msg) {
+			if (LEVEL <= WARN) {
+				Log.w(tag, msg);
+			}
+		}
+		public void e(String tag, String msg) {
+			if (LEVEL <= ERROR) {
+				Log.e(tag, msg);
+			}
+		}
+	}
+
+	// 线程进行网络传输
 	public static void sendHttpRequest(final String address, final HttpCallbackListener listener) {
 		new Thread(new Runnable() {
 			@Override
